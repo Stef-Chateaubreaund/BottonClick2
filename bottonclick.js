e512266b74a328eaa532c7c463618de75a757b41
StefannyDef <stefannydefreitas@gmail.com>
2022-05-24 18:16:13 -0400

    commit

---
+++ bottonclick.js
@@ -0,0 +1,11 @@
+function hide(element) {
+    console.log(element)
+element.remove();
+}
+function troca(element){
+    element.innerText="Log Out"
+
+}
+function ninja(element){
+    alert("Ninja was clicked")
+}
